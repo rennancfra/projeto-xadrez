39a3b56374a314be8e76fe08912a0d4dd373c67a
Rennan Costa Fragozo <rennancfra@outlook.com>
2019-03-21 21:49:52 -0300

    Commit inicial

---
+++ jogo-xadrez-console/jogo-xadrez-console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogo_xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
