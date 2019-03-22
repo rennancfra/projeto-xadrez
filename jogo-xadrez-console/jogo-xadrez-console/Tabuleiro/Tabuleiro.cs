e601e509cc5539877cd13ecc993f5d8672d30852
Rennan Costa Fragozo <rennancfra@outlook.com>
2019-03-22 13:30:17 -0300

    Colocando peças no tabuleiro

--- jogo-xadrez-console/jogo-xadrez-console/Tabuleiro/Tabuleiro.cs
+++ jogo-xadrez-console/jogo-xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -20,5 +20,11 @@ namespace Tabuleiro
         {
             return pecas[linha, coluna];
         }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+        }
     }
 }
---
+++ jogo-xadrez-console/jogo-xadrez-console/Xadrez/Torre.cs
@@ -0,0 +1,17 @@
+﻿using Tabuleiro;
+
+namespace Xadrez
+{
+    class Torre : Peca
+    {
+        public Torre(Tabuleiro.Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
