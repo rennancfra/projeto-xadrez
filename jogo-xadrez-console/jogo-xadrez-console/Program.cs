a10f01bfca265c1c5e19a5846be841b97bcdd65b
Rennan Costa Fragozo <rennancfra@outlook.com>
2019-03-22 14:23:35 -0300

    Melhorando o método ImprimirTabuleiro

--- jogo-xadrez-console/jogo-xadrez-console/Program.cs
+++ jogo-xadrez-console/jogo-xadrez-console/Program.cs
@@ -12,22 +12,24 @@ namespace jogo_xadrez_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            /*PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
             Console.WriteLine(pos.toPosicao());
 
-            Console.ReadLine();
-            /*try
+            Console.ReadLine();*/
+            try
             {
 
             
-            Tabuleiro.Tabuleiro tab = new Tabuleiro.Tabuleiro(8, 8);
+                Tabuleiro.Tabuleiro tab = new Tabuleiro.Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            Tela.imprimirTabuleiro(tab);
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
+                Tela.imprimirTabuleiro(tab);
 
             
             }
@@ -36,7 +38,7 @@ namespace jogo_xadrez_console
 
                 Console.Write(e.Message);
             }
-            Console.ReadKey();*/
+            Console.ReadKey();
         }
     }
 }
